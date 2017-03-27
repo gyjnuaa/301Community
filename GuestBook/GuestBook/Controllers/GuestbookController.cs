d9e22018cd17764599835fdc23d636f39c29e5ba
gaoyanjing <gaoyanjing@youbang.com>
2017-03-27 19:35:03 +0800

    项目框架

---
+++ GuestBook/GuestBook/Controllers/GuestbookController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuestBook.Models;
+
+namespace GuestBook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        // GET: Guestbook
+        private GuestbookContext _db=new GuestbookContext();
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(GuestbookEntry entry)
+        {
+            entry.DateAdded=DateTime.Now;
+            _db.Entries.Add(entry);
+            _db.SaveChanges();
+            return Content("New entry Successfully added");
+        }
+
+        public ActionResult Index()
+        {
+            var mostRecentEntries =
+                (from entry in _db.Entries orderby entry.DateAdded descending select entry).Take(20);
+            var model = mostRecentEntries.ToList();
+            return View(model);
+        }
+
+        public ViewResult Show(int id)
+        {
+            var entry = _db.Entries.Find(id);
+            bool hasPermission = User.Identity.Name == entry.Name;
+            ViewBag.HasPermission = hasPermission;
+            return View(entry);
+        }
+    }
+}
---
+++ GuestBook/GuestBook/Models/GuestbookContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+namespace GuestBook.Models
+{
+    public class GuestbookContext:DbContext
+    {
+        public GuestbookContext() : base("Guestbook")
+        {
+        }
+
+        public DbSet<GuestbookEntry> Entries { get; set; }
+    }
+}
